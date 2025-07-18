c6f1ebb0da73c0601d5bcc2633151a540b5c83b3
guilhermeleeno <guilhermelino@msn.com>
2025-07-17 21:19:32 -0300

    CartasSuperTrunfo.c
    nivel mestre, apanhei muito nesse final... não consegui colocar o nome do atributo não kkk

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
         // Desafio Super Trunfo 
         //Nivel Aventureiro (logica)
@@ -7,18 +8,19 @@
             // Carta 01
             char pais1[20], nome_cidade1[20]; 
             int pontos_turisticos1, populacao1;
-            float area1, pib1, densidade1, pib_per1, dens_inversa1;
+            float area1, pib1, densidade1, pib_per1, dens_inversa1, somac1bat1, somac1bat2;
 
 
             // Carta 02
             char pais2[20], nome_cidade2[20]; 
             int pontos_turisticos2, populacao2;;
-            float area2, pib2, densidade2, pib_per2, dens_inversa2;
+            float area2, pib2, densidade2, pib_per2, dens_inversa2, somac2bat1, somac2bat2;
 
             //Batalha
-            int batalha;
+            int batalha1, batalha2;
+            float soma_carta1, soma_carta2;
 
-        //CARTA 01  input
+        
             printf("CADASTRO DA CARTA 01: \n");
 
             printf("Digite o nome do país onde a cidade da carta está localizada \n");
@@ -80,7 +82,6 @@
             densidade2 = (float) populacao2 / area2;
             dens_inversa2 = (float) 1 / densidade2;
             pib_per2 = (float) pib2 / populacao2;
-            // super_poder2 = (float) pontos_turisticos2 + (float) populacao2 + area2 + pib2 + dens_inversa2 + pib_per2;
 
         //RESULTADOS
             printf("CARTAS CADASTRADAS \n");
@@ -112,85 +113,190 @@
 
             //BATALHA DOS RESULTADOS! 
             printf("---------------------------------------------------\n");
-            printf("Agora escolha um atributo para a batalha do Super Trunfo!\n\n");
+            printf("Agora escolha um atributo para a PRIMEIRA BATALHA do Super Trunfo!\n\n");
 
 printf("1. População\n");
 printf("2. Área\n");
 printf("3. PIB\n");
 printf("4. Número de pontos turísticos\n");
 printf("5. Densidade demográfica\n");
-scanf("%d", &batalha);
+scanf("%d", &batalha1);
 
-switch (batalha) {
-    case 1: // População
-        printf("A batalha vai ser entre: %s (%s) e %s (%s) \n", nome_cidade1, pais1, nome_cidade2, pais2);
-        if (populacao1 > populacao2) {
-            printf("%s venceu a batalha, sua população é de %d \n", nome_cidade1, populacao1);
-            printf("Enquanto %d é a população de %s (%s), que perdeu a batalha.\n", populacao2, nome_cidade2, pais2);
-        } else if (populacao1 == populacao2) {
-            printf("Empate! Ambas as populações são de %d\n", populacao1);
-        } else {
-            printf("%s venceu a batalha. A POPULAÇÃO de %s é %d\n", pais2, nome_cidade2, populacao2);
-            printf("Enquanto %d é a população de %s (%s), que perdeu a batalha.\n", populacao1, nome_cidade1, pais1);
-        }
-        break;
 
-    case 2: // Área
-        printf("A batalha vai ser entre: %s (%s) e %s (%s) \n", nome_cidade1, pais1, nome_cidade2, pais2);
-        if (area1 > area2) {
-            printf("%s venceu a batalha, com ÁREA de %.0f km²\n",nome_cidade1, area1);
-            printf("Enquanto %.0f km² é a área de %s (%s), que perdeu a batalha.\n", area2, nome_cidade2, pais2);
-        } else if (area1 == area2) {
-            printf("Empate! Ambas as áreas são de %.0f km²\n", area1);
-        } else {
-            printf("%s venceu a batalha. A ÁREA de %s é %.0f km²\n", nome_cidade2, area2);
-            printf("Enquanto %.0f km² é a área de %s (%s), que perdeu a batalha.\n", area1, nome_cidade1, pais1);
-        }
-        break;
+switch (batalha1) {
+    case 1: // Pop
+        printf("A batalha vai ser entre: %s (%s) e %s (%s)\n", nome_cidade1, pais1, nome_cidade2, pais2);
+
+        (populacao1 > populacao2) ?
+            printf("A vitória do quesito População é: %s com %d pessoas\n", nome_cidade1, populacao1) :
+            printf("A vitória do quesito População é: %s com %d pessoas\n", nome_cidade2, populacao2);
+        
+            somac1bat1 = populacao1;
+            somac2bat1 = populacao2;
+        
+            break;
+
+    case 2: //area
+        printf("A batalha vai ser entre: %s (%s) e %s (%s)\n", nome_cidade1, pais1, nome_cidade2, pais2);
+
+        (area1 > area2) ?
+            printf("A vitória do quesito Área é: %s com %.2f km²\n", nome_cidade1, area1) :
+            printf("A vitória do quesito Área é: %s com %.2f km²\n", nome_cidade2, area2);
+
+            somac1bat1 = area1;
+            somac2bat1 =  area2;
+     
+            break;
 
     case 3: // PIB
-        printf("A batalha vai ser entre: %s (%s) e %s (%s) \n", nome_cidade1, pais1, nome_cidade2, pais2);
-        if (pib1 > pib2) {
-            printf("%s (%s) venceu a batalha com o PIB de %.0f \n", nome_cidade1, pais1, pib1);
-            printf("Enquanto %.0f  é o PIB de %s (%s), que perdeu a batalha.\n", pib2, nome_cidade2, pais2);
-        } else if (pib1 == pib2) {
-            printf("Empate! Ambos os PIBs são de %.0f \n", pib1);
-        } else {
-            printf("%s venceu a batalha com o PIB de %.0f \n", nome_cidade2, pib2);
-            printf("Enquanto %.0f  é o PIB de %s (%s), que perdeu a batalha.\n", pib1, nome_cidade1, pais1);
-        }
-        break;
+        printf("A batalha vai ser entre: %s (%s) e %s (%s)\n", nome_cidade1, pais1, nome_cidade2, pais2);
 
-    case 4: // Pontos turísticos
-        printf("A batalha vai ser entre: %s (%s) e %s (%s) \n", nome_cidade1, pais1, nome_cidade2, pais2);
-        if (pontos_turisticos1 > pontos_turisticos2) {
-            printf("%s venceu a batalha com %d pontos turísticos\n",nome_cidade1, pontos_turisticos1);
-            printf("Enquanto %s (%s) tem  %d\n", nome_cidade2, pais2, pontos_turisticos2);
-        } else if (pontos_turisticos1 == pontos_turisticos2) {
-            printf("Empate! Ambos têm %d pontos turísticos\n", pontos_turisticos1);
-        } else {
-            printf("%s venceu a batalha com %d pontos turísticos\n", nome_cidade2, pontos_turisticos2);
-            printf("Enquanto %s (%s) tem  %d\n", nome_cidade1, pais1, pontos_turisticos1);
-        }
-        break;
+        (pib1 > pib2) ?
+            printf("A vitória do quesito PIB é: %s com R$ %.2f bilhões\n", nome_cidade1, pib1) :
+            printf("A vitória do quesito PIB é: %s com R$ %.2f bilhões\n", nome_cidade2, pib2);
+            somac1bat1 = pib1;
+            somac2bat1 = pib2;
 
-    case 5: // Densidade demográfica
-        printf("A batalha vai ser entre: %s (%s) e %s (%s) \n", nome_cidade1, pais1, nome_cidade2, pais2);
-        if ( dens_inversa1 < dens_inversa2 ) {
-            printf("%s venceu a batalha com a DENSIDADE DEMOGRÁFICA de %.3f hab/km² (Quanto menor, melhor!)\n", nome_cidade1, densidade1);
-            printf("Enquanto %.3f hab/km² é a densidade de %s (%s)\n", densidade2, nome_cidade2, pais2);
-        } else if (dens_inversa1 == dens_inversa2) {
-            printf("Empate! Ambas as densidades são de %.3f hab/km²\n", densidade1);
-        } else {
-            printf("%s venceu a batalha com a DENSIDADE DEMOGRÁFICA de %.3f hab/km² (Quanto menor, melhor!) \n", nome_cidade2, densidade2);
-            printf("Enquanto %.3f hab/km² é a densidade de %s (%s)\n", densidade1, nome_cidade1, pais1);
-        }
-        break;
+            break;
+
+    case 4: //pontos turísticos
+        printf("A batalha vai ser entre: %s (%s) e %s (%s)\n", nome_cidade1, pais1, nome_cidade2, pais2);
+
+        (pontos_turisticos1 > pontos_turisticos2) ?
+            printf("A vitória do quesito Pontos Turísticos é: %s com %d ponto(s)\n", nome_cidade1, pontos_turisticos1) :
+            printf("A vitória do quesito Pontos Turísticos é: %s com %d ponto(s)\n", nome_cidade2, pontos_turisticos2);
+            somac1bat1 = pontos_turisticos1;
+            somac2bat1 =pontos_turisticos2;
+
+            break;
+
+    case 5: //densidade demográfica
+        printf("A batalha vai ser entre: %s (%s) e %s (%s)\n", nome_cidade1, pais1, nome_cidade2, pais2);
+
+        (dens_inversa1 < dens_inversa2) ?
+            printf("A vitória do quesito Densidade Demográfica é: %s com %.2f pessoas por km²\n", nome_cidade1, densidade1) :
+            printf("A vitória do quesito Densidade Demográfica é: %s com %.2f pessoas por km²\n", nome_cidade2, densidade2);
+        
+            somac1bat1 =dens_inversa1;
+            somac2bat1 =dens_inversa2;
+        
+            break;
 
     default:
         printf("Opção inválida! Escolha de 1 a 5.\n");
         break;
 }
 
+
+printf("---------------------------------------------------\n");
+printf("Agora escolha um atributo para a SEGUNDA BATALHA do Super Trunfo!\n\n");
+
+printf("1. População\n");
+printf("2. Área\n");
+printf("3. PIB\n");
+printf("4. Número de pontos turísticos\n");
+printf("5. Densidade demográfica\n");
+scanf("%d", &batalha2);
+
+if(batalha2 == batalha1){
+    printf("Não escolha o mesmo atributo que o PRIMEIRO para a SEGUNDA batalha");
+    exit(1);
+}
+
+switch (batalha2) {
+    case 1: //Pop
+        printf("A batalha vai ser entre: %s (%s) e %s (%s)\n", nome_cidade1, pais1, nome_cidade2, pais2);
+
+        (populacao1 > populacao2) ?
+            printf("A vitória do quesito População é: %s com %d pessoas\n", nome_cidade1, populacao1) :
+            printf("A vitória do quesito População é: %s com %d pessoas\n", nome_cidade2, populacao2);
+        
+            somac1bat2 =populacao1;
+            somac2bat2 =populacao2;
+        
+            break;
+
+    case 2: //area
+        printf("A batalha vai ser entre: %s (%s) e %s (%s)\n", nome_cidade1, pais1, nome_cidade2, pais2);
+
+        (area1 > area2) ?
+            printf("A vitória do quesito Área é: %s com %.2f km²\n", nome_cidade1, area1) :
+        
+            printf("A vitória do quesito Área é: %s com %.2f km²\n", nome_cidade2, area2);
+            
+            somac1bat2 =area1;
+            somac2bat2 =area2;
+        
+            break;
+
+    case 3: // PIB
+        printf("A batalha vai ser entre: %s (%s) e %s (%s)\n", nome_cidade1, pais1, nome_cidade2, pais2);
+
+        (pib1 > pib2) ?
+            printf("A vitória do quesito PIB é: %s com R$ %.2f bilhões\n", nome_cidade1, pib1) :
+            printf("A vitória do quesito PIB é: %s com R$ %.2f bilhões\n", nome_cidade2, pib2);
+        
+            somac1bat2 = pib1;
+            somac2bat2 = pib2;
+        
+            break;
+
+    case 4: // pontos turísticos
+        printf("A batalha vai ser entre: %s (%s) e %s (%s)\n", nome_cidade1, pais1, nome_cidade2, pais2);
+
+        (pontos_turisticos1 > pontos_turisticos2) ?
+            printf("A vitória do quesito Pontos Turísticos é: %s com %d ponto(s)\n", nome_cidade1, pontos_turisticos1) :
+            printf("A vitória do quesito Pontos Turísticos é: %s com %d ponto(s)\n", nome_cidade2, pontos_turisticos2);
+        
+            somac1bat2 =pontos_turisticos1;
+            somac2bat2 =pontos_turisticos2;
+        
+            break;
+
+    case 5: //densidade demográfica
+        printf("A batalha vai ser entre: %s (%s) e %s (%s)\n", nome_cidade1, pais1, nome_cidade2, pais2);
+
+        (dens_inversa1 < dens_inversa2) ?
+            printf("A vitória do quesito Densidade Demográfica é: %s com %.2f pessoas por km²\n", nome_cidade1, densidade1) :
+            printf("A vitória do quesito Densidade Demográfica é: %s com %.2f pessoas por km²\n", nome_cidade2, densidade2);
+
+            somac1bat2 = dens_inversa1;
+            somac2bat2 = dens_inversa2;
+
+            break;
+
+    default:
+        printf("Opção inválida! Escolha de 1 a 5.\n");
+        break;
+}
+
+//  A soma das pontuações
+soma_carta1 = somac1bat1 + somac1bat2;
+soma_carta2 = somac2bat1 + somac2bat2;
+
+printf("\n===============================================\n");
+printf("RESULTADO FINAL DA BATALHA\n\n");
+
+// Exibe dados da carta 1
+printf("Carta 1 - %s (%s):\n", nome_cidade1, pais1);
+printf("- Atributo da 1ª batalha: %.2f\n", somac1bat1);
+printf("- Atributo da 2ª batalha: %.2f\n", somac1bat2);
+printf("=> Soma total: %.2f\n\n", soma_carta1);
+
+// Exibe dados da carta 2
+printf("Carta 2 - %s (%s):\n", nome_cidade2, pais2);
+printf("- Atributo da 1ª batalha: %.2f\n", somac2bat1);
+printf("- Atributo da 2ª batalha: %.2f\n", somac2bat2);
+printf("=> Soma total: %.2f\n\n", soma_carta2);
+
+if (soma_carta1 > soma_carta2) {
+    printf("A cidade vencedora é: %s (%s) \n", nome_cidade1, pais1);
+} else if (soma_carta2 > soma_carta1) {
+    printf("A cidade vencedora é: %s (%s) \n", nome_cidade2, pais2);
+} else {
+    printf(" Empate! As 2 cidades tiveram a mesma pontuação.\n");
+}
+
+        
         return 0;
-        }
+}
